2d8def61351a01a322af451de53eac9f077fbbaa
McKay Shields <mckaygs@byu.edu>
2025-01-21 09:02:10 -0700

    chess piece code

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +53,47 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList<>();
+        if (type == ChessPiece.PieceType.KING) {
+            moves.addAll(KingMoves(board,myPosition));
+        } else if (type == ChessPiece.PieceType.QUEEN) {
+            moves.addAll(QueenMoves(board,myPosition));
+        } else if (type == ChessPiece.PieceType.BISHOP) {
+            moves.addAll(BishopMoves(board,myPosition));
+        } else if (type == ChessPiece.PieceType.KNIGHT) {
+            moves.addAll(KnightMoves(board,myPosition));
+        } else if (type == ChessPiece.PieceType.ROOK) {
+            moves.addAll(RookMoves(board,myPosition));
+        } else if (type == ChessPiece.PieceType.PAWN) {
+            moves.addAll(PawnMoves(board,myPosition));
+        }
+        return moves;
+    }
+
+    private Collection<ChessMove> KingMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        return moves;
+    }
+    private Collection<ChessMove> QueenMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        moves.addAll(BishopMoves(board, position));
+        moves.addAll(RookMoves(board, position));
+        return moves;
+    }
+    private Collection<ChessMove> BishopMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        return moves;
+    }
+    private Collection<ChessMove> KnightMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        return moves;
+    }
+    private Collection<ChessMove> RookMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        return moves;
+    }
+    private Collection<ChessMove> PawnMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        return moves;
     }
 }
