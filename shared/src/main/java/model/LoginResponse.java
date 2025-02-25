0c07d95032e9b59bf071eabc7c42a6eddcf9e1e9
McKay Shields <mckaygs@byu.edu>
2025-02-24 18:52:29 -0700

    response classes

---
+++ shared/src/main/java/model/LoginResponse.java
@@ -0,0 +1,6 @@
+package model;
+
+public record LoginResponse(String username, String authToken){
+    
+}
+
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Collection;
+
 public record UserData(String username, String password, String email) {
 }
 
