393c110d89d078dace9eb6af85fe33ea6376ce60
McKay Shields <mckaygs@byu.edu>
2025-03-10 13:06:23 -0600

    auth tests start

--- server/src/test/java/dataaccess/AuthDataTests.java
+++ server/src/test/java/dataaccess/AuthDataTests.java
@@ -1,12 +1,34 @@
 package dataaccess;
 
-import model.AuthData;
-import model.UserData;
+import model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthDataTests {
 
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        SqlAuthData authDataAccess = new SqlAuthData();
+        authDataAccess.clear();
+    }
+
+    @Test
+    void testCreateAuth() throws DataAccessException {
+        AuthData authData = new AuthData("testToken", "testUser");
+        AuthDataAccess authDataAccess = new SqlAuthData();
+        authDataAccess.createAuth(authData);
+
+        AuthData retrievedAuth = authDataAccess.getAuth("testToken");
+        assertNotNull(retrievedAuth);
+        assertEquals("testUser", retrievedAuth.username());
+    }
+
+    @Test
+    void createAuthBad() throws DataAccessException {
+        AuthDataAccess authDataAccess = new SqlAuthData();
+        AuthData retrievedAuth = authDataAccess.getAuth("nonExistentToken");
+        assertNull(retrievedAuth);
+    }
 
 }
