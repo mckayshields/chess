ecc906f6fd7d4160a72977501d6bbe1ba44156d8
McKay Shields <mckaygs@byu.edu>
2025-03-18 09:33:56 -0600

    phase 5 getting started

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,22 @@
 import chess.*;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        String whitePieces= "♔♕♖♗♘♙";
+        String blackPieces="♚♛♜♝♞♟";
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLACK);
+        out.println(whitePieces);
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.println(blackPieces);
+
     }
 }
