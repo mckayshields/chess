8355f5f32d881c527cb32375197ffe04ad93cfbe
McKay Shields <mckaygs@byu.edu>
2025-03-19 12:45:27 -0600

    finish drawboard

--- client/src/main/java/DrawBoard.java
+++ client/src/main/java/DrawBoard.java
@@ -8,12 +8,37 @@ import static ui.EscapeSequences.*;
 
 public class DrawBoard {
     private final ChessBoard chessboard;
+    private final boolean isBlackPOV;
+    private final int start;
+    private final int end;
+    private final int direction;
+    private static final String PIECE_PADDING = " ";
 
-    public DrawBoard(ChessBoard chessboard) {
+    public DrawBoard(ChessBoard chessboard, boolean isBlackPOV) {
         this.chessboard = chessboard;
+        this.isBlackPOV = isBlackPOV;
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        for (int row = 1; row <= 8; row++) {
-            for (int col = 1; col <= 8; col++) {
+        out.print(ERASE_SCREEN);
+
+        //ticTacToe(out);
+
+        if (isBlackPOV){
+            start = 1;
+            end = 8;
+            direction = 1;
+        }
+        else{
+            start = 8;
+            end = 1;
+            direction = -1;
+        }
+
+        makeHeader(out);
+        for (int row = start;  isBlackPOV ? row <= end : row >= end; row += direction) {
+            out.print(SET_TEXT_COLOR_BLUE);
+            out.print(SET_BG_COLOR_DARK_GREY);
+            out.print(PIECE_PADDING + row + PIECE_PADDING);
+            for(int col = end;  isBlackPOV ? col >= start : col <= start; col += -1*direction) {
                 if(row % 2 == col % 2){
                     out.print(SET_BG_COLOR_DARK_GREY);
                 }
@@ -23,62 +48,70 @@ public class DrawBoard {
                 ChessPosition position = new ChessPosition(row, col);
                 ChessPiece piece = chessboard.getPiece(position);
                 String pieceCharacter = getUnicode(piece);
-                out.print(pieceCharacter);
+                out.print(PIECE_PADDING + pieceCharacter + PIECE_PADDING);
+                out.print("\u001B[22m");
             }
+            out.print(SET_BG_COLOR_DARK_GREY);
+            out.print(SET_TEXT_COLOR_BLUE);
+            out.print(PIECE_PADDING + row + PIECE_PADDING);
             out.println();
         }
+        makeHeader(out);
     }
 
+
+    private void drawHorizontalLine(PrintStream out) {
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(EMPTY.repeat(24+7));
+            out.println();
+    }
+
+
+
     String getUnicode(ChessPiece piece){
-        String pieceCharacter = " ";
-        if(piece.getPieceType() == null){
+        String pieceCharacter = "\u3000";
+        if(piece == null){
             return pieceCharacter;
         }
         if(piece.getTeamColor() == ChessGame.TeamColor.WHITE){
-            switch (piece.getPieceType()){
-                case PAWN:
-                    pieceCharacter = "♙";
-                    break;
-                case ROOK:
-                    pieceCharacter = "♖";
-                    break;
-                case KNIGHT:
-                    pieceCharacter = "♘";
-                    break;
-                case BISHOP:
-                    pieceCharacter = "♗";
-                    break;
-                case QUEEN:
-                    pieceCharacter = "♕";
-                    break;
-                case KING:
-                    pieceCharacter = "♔";
-                    break;
-            }
+            out.print(SET_TEXT_COLOR_WHITE);
         }
-        else{
-            switch (piece.getPieceType()){
-                case PAWN:
-                    pieceCharacter = "♟";
-                    break;
-                case ROOK:
-                    pieceCharacter = "♜";
-                    break;
-                case KNIGHT:
-                    pieceCharacter = "♞";
-                    break;
-                case BISHOP:
-                    pieceCharacter = "♝";
-                    break;
-                case QUEEN:
-                    pieceCharacter = "♛";
-                    break;
-                case KING:
-                    pieceCharacter = "♚";
-                    break;
-            }
+        else {
+            //out.print(SET_TEXT_BOLD);
+            out.print(SET_TEXT_COLOR_BLACK);
         }
+        pieceCharacter = switch (piece.getPieceType()) {
+            case PAWN -> "♟";
+            case ROOK -> "♜";
+            case KNIGHT -> "♞";
+            case BISHOP -> "♝";
+            case QUEEN -> "♛";
+            case KING -> "♚";
+        };
+        /*pieceCharacter = switch (piece.getPieceType()) {
+            case PAWN -> "P";
+            case ROOK -> "R";
+            case KNIGHT -> "N";
+            case BISHOP -> "B";
+            case QUEEN -> "Q";
+            case KING -> "K";
+        };*/
         return pieceCharacter;
+    }
+
+    void makeHeader(PrintStream out){
+        out.print(PIECE_PADDING + " " + PIECE_PADDING);
+        String[] columns = {"A", "B", "C", "D", "E", "F", "G", "H"};
+        for(int col = end;  isBlackPOV ? col >= start : col <= start; col += -1*direction){
+            out.print(SET_TEXT_COLOR_BLUE);
+            out.print(SET_TEXT_BOLD);
+            out.print("\u2001\u200A" + columns[col-1] + "\u2001");
+            if (col%3 != 0){
+                out.print("\u200A");
+            }
+            out.print("\u001B[22m");
+        }
+        out.println();
     }
 
 }
--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -9,17 +9,8 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        String whitePieces= "♔♕♖♗♘♙";
-        String blackPieces="♚♛♜♝♞♟";
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
-        out.println(whitePieces);
-        out.print(SET_BG_COLOR_DARK_GREY);
-        out.println(blackPieces);
         ChessBoard defaultBoard = new ChessBoard();
         defaultBoard.resetBoard();
-        new DrawBoard(defaultBoard);
-
+        new DrawBoard(defaultBoard, false);
     }
 }
